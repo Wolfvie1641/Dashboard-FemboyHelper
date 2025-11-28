4abcadb862e5ed162f887a7e36496ce9ee2bcbae
Wolfvie1641 <85754497+Wolfvie1641@users.noreply.github.com>
2025-11-28 18:50:44 +0700

    Update LiveLog.jsx

--- src/public/LiveLog.jsx
+++ src/public/LiveLog.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", { transports: ["websocket"] });
+const socket = io("https://backend-femboyhelper-production.up.railway.app", { transports: ["websocket"] });
 socket.emit("identify", "dashboard");
 
 export default function LiveLog() {
